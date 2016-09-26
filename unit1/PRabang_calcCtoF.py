7b96bdb46e9ac0b8ebbd9952ef15027a3f636b21
Phoenix Rabang <phoenixrabang@uagateway.org>
2016-09-26 15:43:30 +0000

    initial commit

---
+++ unit1/PRabang_calcCtoF.py
@@ -0,0 +1,5 @@
+Celsius = int(input("Enter a temperature in Celsius: "))
+
+Fahrenheit = 9/5 * Celsius + 32
+
+print "", Celsius, " Degrees Celsius = ", Fahrenheit, " Degrees Fahrenheit"
---
+++ unit1/PRabang_calcFtoC.py
@@ -0,0 +1,7 @@
+F = int(input("Enter a temperature in Fahrenheit: "))
+
+
+Celsius = (F - 32) * 5/9
+
+
+print (F , " Degrees Fahrenheit = ", Celsius, " Degrees Celsius")
