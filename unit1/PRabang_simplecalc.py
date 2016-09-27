dbac2f59fdc916b54683e00838cecdb9fa6d3d17
Phoenix Rabang <phoenixrabang@uagateway.org>
2016-09-27 16:17:31 +0000

    finish seconds lab

---
+++ unit1/PRabang_simplecalc.py
@@ -0,0 +1,13 @@
+div = "Divsion"
+add = "Additon"
+sub = "Subrtraction"
+multi = "Multipication"
+divv = NumX/NumY
+
+
+NumX = int(input("Whats your first number? "))
+NumY = int(input("Whats your second Number? "))
+op = str(input("What order of opertations? Additon, Subrtraction, Multipication, Division? "))
+
+if op == div
+print("" Divv)
