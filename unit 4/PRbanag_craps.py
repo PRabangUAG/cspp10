1ac3ee4cd5cd4e2c43dbb11f7922874031434c4a
Phoenix Rabang <phoenixrabang@uagateway.org>
2016-12-13 17:16:08 +0000

    Worked on craps

--- unit 4/PRbanag_craps.py
+++ unit 4/PRbanag_craps.py
@@ -2,20 +2,21 @@ import random
 
 
 def get_bet(bank_account):
-    bet=int(input("How much would you like to bet?"))
-    if bet<0:
-        return("your number must be a postive integer and greater than 0")
-    if bet>100:
-        return ("Your bank account balance isnt that higher")
-        bank_account=bank_account
-        return bank_account
-    return bet
+    while True:
+        bet=int(input("How much would you like to bet?"))
+        if bet<0:
+            return("your number must be a postive integer and greater than 0")
+        elif bet>100:
+            return ("Your bank account balance isnt that higher")
+        else:
+            return bet
 
 def rolldice():
     dice1 = random.randint(1,6)
     dice2 = random.randint(1,6)
     dice_sum = dice1 + dice2
     print("rolled 2 dice {} {}".format(dice1,dice2))
+    print("Total:{}".format(dice1 + dice2))
     return rolldice()
     
 def first_roll(dice_sum):
@@ -26,4 +27,23 @@ def first_roll(dice_sum):
     else:
         return dice_sum
     
-def second_roll 
+def second_roll(dice_sum,point_roll):
+    if dice_sum ==7 or dice_sum ==11:
+        return("you win")
+    if dice_sum == 2 or dice_sum == 3 or dice_sum == 12:
+        return ("you lose")
+
+
+
+def craps():
+    bank_account = 100
+    get_bet(bank_account)
+    first_rol = first_roll(dice_sum)
+    if first_rol == "you win":
+        print("you win")
+    elif first_rol == "you lose":
+        print("you lose")
+    else:
+        print("point roll")
+        
+craps()
