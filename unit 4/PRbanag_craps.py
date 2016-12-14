62ea09bd0b213161d128ff9ffcbfe6d8ce88d282
Phoenix Rabang <phoenixrabang@uagateway.org>
2016-12-14 17:16:54 +0000

    workedd on lab

--- unit 4/PRbanag_craps.py
+++ unit 4/PRbanag_craps.py
@@ -1,13 +1,13 @@
 import random
 
-
+#This is youur bet, it tells you if you have enough or not, asking how much you would like to bet
 def get_bet(bank_account):
     while True:
         bet=int(input("How much would you like to bet?"))
         if bet<0:
-            return("your number must be a postive integer and greater than 0")
+            print("your number must be a postive integer and greater than 0")
         elif bet>100:
-            return ("Your bank account balance isnt that higher")
+            print ("Your bank account balance isnt that higher")
         else:
             return bet
 
@@ -17,33 +17,42 @@ def rolldice():
     dice_sum = dice1 + dice2
     print("rolled 2 dice {} {}".format(dice1,dice2))
     print("Total:{}".format(dice1 + dice2))
-    return rolldice()
+    return dice_sum()
     
 def first_roll(dice_sum):
     if dice_sum == 7 or dice_sum == 11:
-        return("you win")
-    if dice_sum == 2 or dice_sum == 3 or dice_sum == 12:
-        return ("you lose")
+        print("you win")
+    elif dice_sum == 2 or dice_sum == 3 or dice_sum == 12:
+        print ("you lose")
     else:
         return dice_sum
     
 def second_roll(dice_sum,point_roll):
     if dice_sum ==7 or dice_sum ==11:
-        return("you win")
-    if dice_sum == 2 or dice_sum == 3 or dice_sum == 12:
-        return ("you lose")
-
-
+        print("you win")
+    elif dice_sum == 2 or dice_sum == 3 or dice_sum == 12:
+        print ("you lose")
+    else:
+        print("try again")
+    return second_roll(dice_sum,point_roll)
+    
+def point_roll(roll, point_roll):
+    while (roll != 7 and roll != point_roll):
+        
+ 
 
 def craps():
     bank_account = 100
     get_bet(bank_account)
-    first_rol = first_roll(dice_sum)
-    if first_rol == "you win":
+    diceroll = rolldice()
+    first_rols = first_roll(dice_sum)
+    if first_rols == "you win":
         print("you win")
-    elif first_rol == "you lose":
+    elif first_rols == "you lose":
         print("you lose")
     else:
         print("point roll")
+        dice = rolldice()
+        point_roll_score = second_roll(dice_sum,first_())
         
 craps()
