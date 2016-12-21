d737116901a6ee8d4f0ba9b9e5170ed46f7363ff
Phoenix Rabang <phoenixrabang@uagateway.org>
2016-12-21 17:15:20 +0000

    worked on fucntions

--- unit 4/PRbanag_craps.py
+++ unit 4/PRbanag_craps.py
@@ -1,8 +1,9 @@
 import random
 
-#This is youur bet, it tells you if you have enough or not, asking how much you would like to bet
+        #This is youur bet, it tells you if you have enough or not, asking how much you would like to bet
 def get_bet(bank_account):
     while True:
+        print("you have ${} in your bank account".format(bank_account))
         bet=int(input("How much would you like to bet? $"))
         if bet<0:
             print("your number must be a postive integer and greater than 0")
@@ -11,6 +12,7 @@ def get_bet(bank_account):
         else:
             return bet
 
+        #getting 2 random numbers 1-6
 def roll2dice():
     dice1 = random.randint(1,6)
     dice2 = random.randint(1,6)
@@ -19,16 +21,16 @@ def roll2dice():
     print("Total:{}".format(dice1 + dice2))
     return dice_sum
     
+        #first round if the player gets a 7 or 11 they win, if they get 2,3,12 they lose ends the game end of round, round restarts ask for players bet again.
 def first_dice_result(dice_sum):
     if dice_sum == 7 or dice_sum == 11:
         print("you win")
-        return(craps())
     elif dice_sum == 2 or dice_sum == 3 or dice_sum == 12:
         print ("you lose")
-        return(craps())
     else:
         return dice_sum
-    
+        
+        #second round if the numbers are not 7,11,2,3,12 it then goes into this function to keep rolling until the first roll numbers or 7
 def second_roll_result(dice_sum,point_roll):
     if dice_sum == point_roll:
         print("you win")
@@ -41,7 +43,7 @@ def second_roll_result(dice_sum,point_roll):
     
 
 
-        
+        #main game!
  
 def craps():
     bank_account = 100
@@ -56,6 +58,5 @@ def craps():
         print("point roll")
         diceroll = roll2dice()
         point_roll_result = second_roll_result(dice,first_result)
-    return(craps())
         
 craps()
