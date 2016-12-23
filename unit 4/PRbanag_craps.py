1f191e4d2ce40dca07c9ead4769bec0d6eebd340
Phoenix Rabang <phoenixrabang@uagateway.org>
2016-12-23 17:13:04 +0000

    worked on craps

--- unit 4/PRbanag_craps.py
+++ unit 4/PRbanag_craps.py
@@ -24,21 +24,19 @@ def roll2dice():
         #first round if the player gets a 7 or 11 they win, if they get 2,3,12 they lose ends the game end of round, round restarts ask for players bet again.
 def first_dice_result(dice_sum):
     if dice_sum == 7 or dice_sum == 11:
-        print("you win")
         return "you win"
     elif dice_sum == 2 or dice_sum == 3 or dice_sum == 12:
-        print ("you lose")
         return "you lose"
     else:
         return dice_sum
         
-    
         #second round if the numbers are not 7,11,2,3,12 it then goes into this function to keep rolling until the first roll numbers or 7
 def second_roll_result(dice_sum,point_roll):
-    if dice_sum == point_roll:
-        print("you win")
-    elif dice_sum == 7:
-        print("you lose")
+    if dice_sum == 7:
+        return "you lose"
+    elif dice_sum == point_roll:
+        return"you win!"
+        
     else:
         while (dice_sum != 7 and dice_sum != point_roll):
             dice_sum=roll2dice()
@@ -61,5 +59,11 @@ def craps():
         print("point roll")
         diceroll = roll2dice()
         point_roll_result = second_roll_result(dice,first_result)
+        print ("__________________________________")
+        while bank_account > 0:
+            if bank_account <= 0:
+                return "You broke as shit"
+            elif bank_account > 0:
+                return craps()
         
 craps()
