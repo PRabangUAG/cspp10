ab3e95876c0f4a0c89b8df75d5bdec1a4c8714ea
Phoenix Rabang <phoenixrabang@uagateway.org>
2017-02-02 17:13:30 +0000

    sudo code day 2

---
+++ unit 5/PRabang_scramble.py
@@ -0,0 +1,11 @@
+import random
+alist = []
+word = ""
+#getting users input
+#make a list
+
+#making users input = to a list
+
+#scrambling the list that you made with the users input
+
+#printing users input
