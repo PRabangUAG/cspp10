85005174ca7aa15cad8e5537f608b09d62b77f05
Phoenix Rabang <phoenixrabang@uagateway.org>
2017-02-06 17:17:22 +0000

    didnt finish the project :)

--- unit 5/PRabang_scramble.py
+++ unit 5/PRabang_scramble.py
@@ -1,9 +1,24 @@
 import random
-alist = []
-word = ""
+
+word = "orange"
+def scramble_word(word):
+ if len(word) > 3:
+    alist = list(word)
+    firstletter = word[1]
+    lastletter = word [-1]
+    xlist = word[1:-1]
+    random.shuffle(xlist)
+    xlist.insert(0,firstletter)
+    xlist.append(lastletter)
+    final =''.join(xlist)
+    print(final)
+    
+    
+scramble_word(word)
+    
 #getting users input
 #make a list
-
+ 
 #making users input = to a list
 
 #scrambling the list that you made with the users input
